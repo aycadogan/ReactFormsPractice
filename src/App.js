8cf8ecdf1b5936e4c60fc710e72d4204567de248
aycadogan89 <doganayca89@gmail.com>
2021-11-10 22:18:20 -0800

    my form app

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {useState, useRef} from 'react';
+
+function App() {
+  const [nameInput, setNameInput] = useState('')
+  const inputRef = useRef()
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //console.log(nameInput)
+    console.log(inputRef.current.value)
+  }
+
+  const handleInputChange = (e) => {
+    //console.log(e.target.value)
+    setNameInput(e.target.value)
+  }
+
+  return (
+    <div className="App">
+      <h1>React Practice Forms</h1>
+      <h2>Event, Forms and Ref</h2>
+
+      <br />
+
+      <form onSubmit={handleSubmit}>
+        <div className="group">
+          <input 
+            type="text" 
+            onBlur={ () => console.log('Blurred')}
+            onFocus={ () => console.log('Focused')}
+            //onChange = { () => console.log('Changed')}
+            //onChange = { handleInputChange }
+            //onChange={(e) => setNameInput(e.target.value)}
+            ref={inputRef}
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
